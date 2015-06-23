dd07f61483b43c94c769a6b25d25e53c69a55530
Samuel Mello <samuel@amellostyle.com>
2015-06-23 18:33:52 -0400

    updated plugin name

--- admin-command-palette/trunk/admin-command-palette.php
+++ admin-command-palette/trunk/admin-command-palette.php
@@ -13,12 +13,12 @@
  * @package           Admin_Command_Palette
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
+ * Plugin Name:       Admin Command Palette
  * Plugin URI:        http://example.com/admin-command-palette-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Clark Nildek Powell
+ * Author URI:        http://clarknikdelpowell.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       admin-command-palette
