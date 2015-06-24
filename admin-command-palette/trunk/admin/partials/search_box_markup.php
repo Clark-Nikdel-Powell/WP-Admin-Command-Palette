fc0fae3e3d7c03315000bc1be05717d9f2602c4a
Josh Nederveld <joshnederveld@gmail.com>
2015-06-23 21:43:30 -0400

    Structure and styles

--- admin-command-palette/trunk/admin/partials/search_box_markup.php
+++ admin-command-palette/trunk/admin/partials/search_box_markup.php
@@ -1,5 +1,8 @@
 <div id="admin-command-palette-overlay"></div>
-<div id="admin-command-palette">
-	<input type="search" placeholder="Start typing..." />
+<div id="admin-command-palette-modal">
+	<div class="search-container">
+		<input type="search" placeholder="Start typing..." />
+		<span class="spinner"></span>
+	</div>
 	<div class="admin-command-palette-results"></div>
 </div>
