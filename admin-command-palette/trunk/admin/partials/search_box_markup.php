dfb8a8b0d0d0915a8dd96eb5cce4929307e59fd4
Josh Nederveld <joshnederveld@gmail.com>
2015-06-23 22:18:22 -0400

    CSS/HTML updates

--- admin-command-palette/trunk/admin/partials/search_box_markup.php
+++ admin-command-palette/trunk/admin/partials/search_box_markup.php
@@ -1,8 +1,7 @@
-<div id="admin-command-palette-overlay"></div>
-<div id="admin-command-palette-modal">
+<div class="admin-command-palette admin-command-palette-overlay"></div>
+<div class="admin-command-palette admin-command-palette-modal">
 	<div class="search-container">
 		<input type="search" placeholder="Start typing..." />
-		<span class="spinner"></span>
 	</div>
 	<div class="admin-command-palette-results"></div>
 </div>
