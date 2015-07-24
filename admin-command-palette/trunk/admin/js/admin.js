1292585eb94db580a910490f6cf2a09194c333c8
Josh Nederveld <joshnederveld@gmail.com>
2015-07-23 21:41:07 -0400

    Removed debug code

--- admin-command-palette/trunk/admin/js/admin.js
+++ admin-command-palette/trunk/admin/js/admin.js
@@ -18196,10 +18196,9 @@ var results_format = 'flat';
 // Update the settings if the data is there.
 if ( 'undefined' !== typeof acp_user_options ) {
 	if ( '' !== acp_user_options.threshold ) {
-		acp_fuse_options.threshold = acp_user_options.threshold;		
+		acp_fuse_options.threshold = acp_user_options.threshold;
 	}
 	if ( 'grouped' === acp_user_options.results_format ) {
-		console.log('YES');
 		results_format = 'grouped';
 	}
 }
@@ -18245,8 +18244,6 @@ $('.admin-command-palette input[type=search]').keyup( function(e) {
 		success : function(response) {
 			var acp_search_data = response;
 
-			console.log(acp_search_data);
-
 			// Search using Fuse
 			var acp_search = new Fuse(acp_search_data, acp_fuse_options);
 
@@ -18287,7 +18284,7 @@ function resultsFlat(acp_result) {
 
 	// The template for each item
 	var template = '{{#results}}{{#if isAction}}<li data-target="{{target}}" data-action="{{action}}">{{title}} <kbd>{{shortcut}}</kbd></li>{{/if}}{{#if !isAction}}<li><a href="{{url}}">{{title}}</a><span><small>{{name}}</small></span></li>{{/if}}{{/results}}';
-	
+
 	// Add the results to the list.
 	var list = '.acp-results .acp-list';
 	var ractive = new Ractive({
@@ -18301,7 +18298,7 @@ function resultsFlat(acp_result) {
 
 	// Find the section and unhide it
 	var section = '.acp-results';
-	$(section).removeClass('hide');	
+	$(section).removeClass('hide');
 }
 
 function resultsGrouped(acp_result) {
