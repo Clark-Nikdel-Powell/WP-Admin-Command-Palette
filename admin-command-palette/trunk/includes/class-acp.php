854e970590849cb87e9844ff111c2ce1865b21ba
Glenn Welser <glenn.welser@gmail.com>
2015-08-20 20:17:32 -0400

    closes #27 - Clear user transients when plugins are de/activated, posts and terms added

--- admin-command-palette/trunk/includes/class-acp.php
+++ admin-command-palette/trunk/includes/class-acp.php
@@ -193,6 +193,11 @@ class ACP {
 		$this->loader->add_action( 'admin_enqueue_scripts', $this->admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $this->admin, 'enqueue_scripts' );
 
+		$this->loader->add_action( 'activated_plugin', $this->admin, 'clear_all_cache' );
+		$this->loader->add_action( 'deactivated_plugin', $this->admin, 'clear_all_cache' );
+		$this->loader->add_action( 'wp_insert_post', $this->admin, 'clear_all_cache' );
+		$this->loader->add_action( 'created_term', $this->admin, 'clear_all_cache' );
+
 	}
 
 	/**
